424d3a7cafdaf38493e3e33dd3043d4f4bb4f273
Charlie Sahlman <csahlman@gmail.com>
2012-12-04 19:30:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BssNewPortal::Application.config.secret_token = 'e91ca234c3d877aae3efaa2433963a71d9495f9ffd6050a9f0e8d141ef1d5d98e8b72324e4371ae039fa5e9280cf7dde7e9b4cc7f2331c61f11d1a80dce0ccb8'
