390673922b53fddf3f15069fb41dc3fd6cf35052
Charlie Sahlman <csahlman@gmail.com>
2012-12-08 16:46:18 -0500

    Added some styling.. sort of

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,7 @@ class PagesController < ApplicationController
   skip_before_filter :authenticate_admin!, only: [ :home ]
 
   def home
-    @development = Track.includes(:lessons).find_by_name("Development")
-    @marketing = Track.includes(:lessons).find_by_name("Marketing")
-    @product = Track.includes(:lessons).find_by_name("Product")
-    @sales = Track.includes(:lessons).find_by_name("Sales")
+    @tracks = Track.includes(:lessons).all
     @semester = Semester.first    
   end
 
