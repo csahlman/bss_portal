e59c0ab2848493992234f2e2ace76746f8f2164b
Charlie Sahlman <csahlman@gmail.com>
2013-01-02 13:19:01 -0500

    Edit/update for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 
   def edit
-    
+    @user = User.find(params[:id])
   end
 
   def update
-    
+    @user = User.find(params[:id])
+    @user.set_user_attributes(params[:user])
+    if @user.save
+      redirect_to root_path, flash: { success: "Updated successfully" }
+    else
+      render 'edit'
+    end
   end
 
 end
