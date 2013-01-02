1831a8d77a2d94306fee8cbfdf7aaebd5106607c
Charlie Sahlman <csahlman@gmail.com>
2013-01-02 13:51:37 -0500

    Added update and edit for days on admin side

--- app/controllers/admin/days_controller.rb
+++ app/controllers/admin/days_controller.rb
@@ -13,4 +13,21 @@ class Admin::DaysController < Admin::BaseController
     @day.destroy
   end
 
+  def edit
+    @semester = Semester.find(params[:semester_id])
+    @day = Day.find(params[:id])
+  end
+
+  def update
+    @semester = Semester.find(params[:semester_id])
+    @day = Day.find(params[:id])
+    @day.date = params[:day][:date]
+    @day.day_value = params[:day][:day_value]
+    if @day.save
+      redirect_to [:admin, @semester], flash: { success: "Edited day successfully" }
+    else
+      render 'edit'
+    end
+  end
+
 end
