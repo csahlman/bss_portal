6d83d8dc3e3f33b3fce4cebbcfabdc0fdd2a84fe
Charlie Sahlman <csahlman@gmail.com>
2013-01-04 17:54:17 -0500

    not a clue

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -8,20 +8,28 @@ class Admin::ImagesController < Admin::BaseController
 
 
   def create
-    @lesson = Lesson.find(params[:lesson_id])
-    @semester = Semester.find(params[:semester_id])
-    @image = @lesson.images.new(params[:image])
-    if @image.save
-      redirect_to admin_semester_lesson_images_path(@semester, @lesson),
-        flash: { success: "Created image" }
+    if params[:semester_id]
+      @lesson = Lesson.find(params[:lesson_id])
+      @semester = Semester.find(params[:semester_id])
+      @image = @lesson.images.new(params[:image])
+      if @image.save
+        redirect_to admin_semester_lesson_images_path(@semester, @lesson),
+          flash: { success: "Created image" }
+      else
+        render 'index'
+      end
     else
-      render 'index'
+      @lesson_template = LessonTemplate.find(params[:lesson_template_id])
+      @image = @lesson_template.images.new(params[:image])
+      if @image.save
+        redirect_to [:edit, :admin, @lesson_template], flash: { success: "Created image" }
+      else
+        
+      end
     end
   end
 
   def destroy
-    @lesson = Lesson.find(params[:lesson_id])
-    @semester = Semester.find(params[:semester_id])
     @image = Image.find(params[:id])
     @image.destroy
   end
