79274638f3fcf5be9c765acb1b1e0c148123af88
Charlie Sahlman <csahlman@gmail.com>
2013-01-08 15:36:11 -0500

    trying to get assets to compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ BssNewPortal::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jquery-1.7.js, fullcalender.css, uniform.css, select2.css, unicorn.main.css, unicorn.blue.css )
+  # config.assets.precompile += %w( jquery-1.7.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
