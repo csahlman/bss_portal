ed5bd29aad3dc51a4872b2a5fa19cf8105e23177
Charlie Sahlman <csahlman@gmail.com>
2013-01-17 11:18:02 -0500

    forms for attachment url

--- app/controllers/admin/attachments_controller.rb
+++ app/controllers/admin/attachments_controller.rb
@@ -2,17 +2,15 @@ class Admin::AttachmentsController < Admin::BaseController
 
   def index
     @lesson = Lesson.find(params[:lesson_id])
-    @semester = Semester.find(params[:semester_id])
     @attachments = @lesson.attachments
   end
 
   def create
     if params[:lesson_id]
       @lesson = Lesson.find(params[:lesson_id])
-      @semester = Semester.find(params[:semester_id])
       @attachment = @lesson.attachments.new(params[:attachment])
       if @attachment.save
-        redirect_to admin_semester_lesson_attachments_path(@semester, @lesson),
+        redirect_to admin_lesson_attachments_path(@lesson),
           flash: { success: "Created attachment" }
       else
         render 'index'
@@ -33,7 +31,6 @@ class Admin::AttachmentsController < Admin::BaseController
 
   def update
     @lesson = Lesson.find(params[:lesson_id])
-    @semester = Semester.find(params[:semester_id])
     @attachment = Attachment.find(params[:id])
   end
 
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -13,13 +13,37 @@
 #  lesson_template_id    :integer
 #
 
-class Attachment < ActiveRecord::Base
-  # attr_accessible :title, :body
+class Attachment < ActiveRecord::Base  
+  attr_accessible :document, :document_url
+
+  attr_accessor :document_url
+
   belongs_to :lesson
   belongs_to :lesson_template
-  attr_accessible :document
   has_attached_file :document
 
   validates_attachment :document, :presence => true,
     :size => { :in => 0..5000.kilobytes }
+
+  before_validation :download_remote_file, if: :document_url_provided?
+
+  private
+
+    def document_url_provided?
+      self.document_url.present?         
+    end  
+
+    def download_remote_file
+      self.document = store_remote_file
+    end
+
+    def store_remote_file
+      io = open(URI.parse(document_url))
+      def io.original_filename
+        base_uri.path.split('/').last
+      end
+      io.original_filename.blank? ? nil : io
+        rescue Exception => exc
+        logger.debug "ERROR WHEN DOWNLOADING FROM #{document_url} - ERROR IS #{exc.message}"
+    end  
 end
